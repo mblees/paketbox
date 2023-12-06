c7046027e3f54f279c9e6cee5dbb0e694e2b126c
mblees <104490472+mblees@users.noreply.github.com>
2023-12-06 14:09:45 +0100

    test main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
-void setup() {
+#include "reed_sensor.h"
+#include "supersonic_sensor.h"
 
+void setup() {
+    Serial.begin(115200);
+
+    init_reed();
+    init_us();
 }
 
 void loop() {
-
+    Serial.print(read_reed());
+    Serial.print(",");
+    Serial.println(read_us());
+    delay(300); // min delay 200ms because of ultrasonic sensor
 }

640ef96ef77aecf5e036e66dbbb0ab4e9468d6f1
mblees <104490472+mblees@users.noreply.github.com>
2023-12-06 14:02:04 +0100

    created files for sensors

---
+++ include/reed_sensor.h
@@ -0,0 +1,18 @@
+#ifndef REED_SENSOR_H
+#define REED_SENSOR_H
+
+#include <stdint.h>
+#include <Arduino.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_DISTANCE 300
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // REED_SENSOR_H
---
+++ include/supersonic_sensor.h
@@ -0,0 +1,18 @@
+#ifndef ULTRASONIC_SENSOR_H
+#define ULTRASONIC_SENSOR_H
+
+#include <stdint.h>
+#include <Arduino.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MAX_DISTANCE 300
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ULTRASONIC_SENSOR_H
